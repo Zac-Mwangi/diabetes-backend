c28e2d52dfde2cd4f88ac685119b805e5cab5e06
ragnar <zackmwangi998@gmail.com>
2023-04-28 19:45:01 +0300

    get_user

--- diabetes_app/urls.py
+++ diabetes_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import ReportListCreateView, ReportRetrieveUpdateDestroyView
 
+from .views import *
+
+
 app_name = 'reports'
 
 urlpatterns = [
     path('reports', ReportListCreateView.as_view(), name='report-list-create'),
+    path('reports_user/<int:pk>', User_Reports, name='User_Reports'),
     path('reports/<int:pk>', ReportRetrieveUpdateDestroyView.as_view(), name='report-detail'),
 ]
--- diabetes_app/views.py
+++ diabetes_app/views.py
@@ -2,6 +2,11 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import ReportModel
 from .serializers import ReportSerializer
 
+from rest_framework.response import Response
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 class ReportListCreateView(ListCreateAPIView):
 
 # bypass authentication
@@ -17,3 +22,21 @@ class ReportRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
     queryset = ReportModel.objects.all()
     serializer_class = ReportSerializer
+
+
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def User_Reports(request, pk):
+    """
+    List all question answers for answered by a particular user
+    """
+    user_entry = ReportModel.objects.filter(user=pk)
+
+    if user_entry.exists():
+        serializer = ReportSerializer(user_entry, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({"message": "No reports found for the specified user."}, status=status.HTTP_404_NOT_FOUND)
